cfd95a6074066ba2e51568112627a680fae1d045
OleksiiKruhlyk <oleksii.kruhlyk@lendio.com>
2025-03-12 16:07:23 +0200

    Add Docker setup and PostgreSQL integration
    Configured Docker Compose for API and PostgreSQL services. Integrated environment variable management with `.env` support and configured database connection using Entity Framework Core. Adjusted project structure and dependencies to support the setup.

--- KomunalkaAPI/Program.cs
+++ KomunalkaAPI/Program.cs
@@ -1,4 +1,27 @@
+using DotNetEnv;
+using KomunalkaAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+Env.Load();
+
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+// DB context connection
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    {
+        var connectionString =
+            $"Host={Env.GetString("DB_HOST")};Database={Env.GetString("DB_NAME")};Username={Env.GetString("DB_USER")};Password={Env.GetString("DB_PASS")}";
+        options.UseNpgsql(connectionString);
+    }
+);
 
 // Add services to the container.
 
