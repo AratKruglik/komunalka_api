fed0daeb44f0be953cb446fa1feba792ef8a57c8
Oleksii Kruhlyk <oleksii.kruhlyk@gmail.com>
2025-08-07 14:46:33 +0300

    Enhance JWT configuration and update user model
    This commit adds JWT settings to the development configuration, including secret, issuer, audience, expiration times, and refresh token settings. Additionally, it introduces a new migration to update the Users table by adding a Role column, facilitating role-based access control. The email format in the API documentation has also been updated for testing purposes.

--- KomunalkaAPI/obj/Debug/net9.0/KomunalkaAPI.AssemblyInfo.cs
+++ KomunalkaAPI/obj/Debug/net9.0/KomunalkaAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KomunalkaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81fee178a128fd51e6ffe88a2bdd362e7a480c49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+214c3d89aa5214eeed4f0596cd85286d54956227")]
 [assembly: System.Reflection.AssemblyProductAttribute("KomunalkaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KomunalkaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
