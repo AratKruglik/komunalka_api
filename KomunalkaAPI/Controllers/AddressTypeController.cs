e5d1351ff4600ac3be1576ef86f24de4d3af67b5
Oleksii Kruhlyk <oleksii.kruhlyk@gmail.com>
2025-08-07 18:58:00 +0300

    Add AddressType and Region models with corresponding repositories and controllers
    - Introduced `AddressType` and `Region` models with properties for name, description, and timestamps.
    - Created `AddressTypeController` and `RegionController` for managing address types and regions via API endpoints.
    - Implemented `IAddressTypeRepository` and `IRegionRepository` interfaces along with their respective repository classes for data access.
    - Added migrations to update the database schema, including new tables for address types and regions, and modifications to the existing `Addresses` table.
    - Enhanced DTOs for address types and regions to facilitate data transfer in API responses.
    - Updated API documentation to include new endpoints for address type and region management.

---
+++ KomunalkaAPI/Controllers/AddressTypeController.cs
@@ -0,0 +1,63 @@
+using KomunalkaAPI.DTO;
+using KomunalkaAPI.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KomunalkaAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class AddressTypeController(IUnitOfWork unitOfWork) : ControllerBase
+{
+    [HttpGet(Name = "addressTypes")]
+    public async Task<ActionResult<IEnumerable<AddressTypeDto>>> GetAll()
+    {
+        var addressTypes = await unitOfWork.AddressTypes.GetAllAsync();
+        var addressTypeList = addressTypes.ToList();
+
+        if (!addressTypeList.Any())
+        {
+            return NotFound("Типи адрес не знайдено");
+        }
+
+        await unitOfWork.CompleteAsync();
+
+        var addressTypeDtos = addressTypeList.Select(addressType => new AddressTypeDto
+        {
+            Id = addressType.Id,
+            Name = addressType.Name,
+            Description = addressType.Description,
+            Icon = addressType.Icon,
+            CreatedAt = addressType.CreatedAt,
+            UpdatedAt = addressType.UpdatedAt
+        }).ToList();
+
+        return Ok(addressTypeDtos);
+    }
+
+    [HttpGet("{id:int}", Name = "addressType")]
+    public async Task<ActionResult<AddressTypeDto>> GetById(int id)
+    {
+        var addressType = await unitOfWork.AddressTypes.GetByIdAsync(id);
+
+        if (addressType == null)
+        {
+            return NotFound("Тип адреси не знайдено");
+        }
+
+        await unitOfWork.CompleteAsync();
+
+        var addressTypeDto = new AddressTypeDto
+        {
+            Id = addressType.Id,
+            Name = addressType.Name,
+            Description = addressType.Description,
+            Icon = addressType.Icon,
+            CreatedAt = addressType.CreatedAt,
+            UpdatedAt = addressType.UpdatedAt
+        };
+
+        return Ok(addressTypeDto);
+    }
+}
