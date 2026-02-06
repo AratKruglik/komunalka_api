a481a8cc603008caa48dcfc3644d4059c9ed4a9b
Oleksii Kruhlyk <oleksii.kruhlyk@gmail.com>
2026-02-06 15:42:46 +0200

    Merge pull request #53
    * feat: add `address_id` column to `service_providers`
    
    * feat: add `utility_type_id` column to `service_providers`
    
    * feat: link service providers to utility types
    
    * feat: add `tariff_id` column to `meter_readings`
    
    * feat: make `UtilityTypeId`, `CurrencyId`, and `EffectiveFrom` optiona…
    
    * feat: seed initial currencies data
    
    * Merge remote-tracking branch 'origin/develop' into feature/providers-…
    
    * Merge branch 'develop' into feature/providers-endpoints
    
    * Merge remote-tracking branch 'origin/feature/providers-endpoints' int…
    
    * Merge branch 'develop' into feature/providers-endpoints
    
    * refactor: rename `pricing_model` to `name` in `tariffs` table and adj…

---
+++ KomunalkaAPI/Migrations/20260206123334_RenameTariffPricingModelToName.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KomunalkaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameTariffPricingModelToName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "pricing_model",
+                table: "tariffs",
+                newName: "name");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "tariffs",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "tariffs",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "tariffs",
+                newName: "pricing_model");
+        }
+    }
+}
